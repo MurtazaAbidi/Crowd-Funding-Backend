bfc6a14fb79261809ca2c7b82f4452378bb97288
Murtaza Abidi <murtaza2476175@gmail.com>
2023-04-25 20:22:17 +0500

    Increases Request's Limit size to 50mb max

--- index.js
+++ index.js
@@ -67,8 +67,9 @@ sqlConn.connection
 //   ],
 // };
 // app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+
 app.set("view engine", "ejs");
 
 // Sample Api ------------------------------------------------------
